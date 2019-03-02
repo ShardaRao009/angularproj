9527c1c74abf4d02393a67d9a82e7abffeb55c36
Sharda <p.sharda009@gmail.com>
2019-03-02 10:04:00 +0530

    @Output king bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { SecondComponentComponent } from './second-component/second-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    FirstComponentComponent,
+    SecondComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/second-component/second-component.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-second-component',
+  templateUrl: './second-component.component.html',
+  styleUrls: ['./second-component.component.css']
+})
+export class SecondComponentComponent implements OnInit {
+  
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
