a05ef80cef658f56c5927ed020451796d24dd951
Sharda <p.sharda009@gmail.com>
2019-03-03 09:27:38 +0530

    css design part - @input with ng-for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,12 @@ export class AppComponent {
   title = 'Welcome to BlogBook';
   test="a";
   
-  rootTest(data){
-    console.log(data);
-  }
-
+  postComponentJson=[{userName:'cally',userImage:'path',post:'Hey feeling bored with Angular',commentsCount:'17',likes:'25'},
+  {userName:'jelly',userImage:'path',post:'Hey I am Enjoying Angular',commentsCount:'7',likes:'5'},
+  {userName:'kukku',userImage:'path',post:'Paitence is the key to success',commentsCount:'1',likes:'2'}]
 }
+  // rootTest(data){
+  //   console.log(data);
+  // }
+
+
